3b496ca6dd850b9958b5e9e3430c9a95518a1bca
Ricardo Souza <Ricardo.Souza@smartm.com>
2022-02-18 11:55:41 -0300

    Added Blazor FlexGrid component for ExpenseOverview

--- BethanysPieShopHRM.UI/Startup.cs
+++ BethanysPieShopHRM.UI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BethanysPieShopHRM.UI.Services;
 using BethanysPieShopHRM.UI.Data;
+using Blazor.FlexGrid;
+using BethanysPieShopHRM.UI.Pages;
 
 namespace BethanysPieShopHRM.UI
 {
@@ -40,6 +42,11 @@ namespace BethanysPieShopHRM.UI
             services.AddScoped<IEmailService, EmailService>();
             services.AddScoped<ISurveyDataService, SurveyDataService>();
             services.AddTransient<IExpenseApprovalService, ExpenseApprovalService>();
+
+            services.AddFlexGridServerSide(cfg =>
+            {
+                cfg.ApplyConfiguration(new ExpenseGridConfiguration());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,7 +65,7 @@ namespace BethanysPieShopHRM.UI
 
             // app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseFlexGrid(env.WebRootPath);
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
